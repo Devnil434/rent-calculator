bc71572c7b4ce7c07a273d731c1bb214e085370d
DevNil <162623650+Devnil434@users.noreply.github.com>
2024-11-10 22:18:10 +0530

    Update rent.py

--- rent.py
+++ rent.py
@@ -1,12 +1,12 @@
 # # Rent Calculator
 
-# rent = int(input("Enter your hostel/flat rent = "))
-# food = int(input("Enter the amount of food ordered = "))
-# electricity_spend = int(input("Electricity you've spent = "))
-# charge_per_unit = int(input("Enter the charge per unit = "))
-# persons = int(input("Enter the no of persons living in room/flat = "))
+rent = int(input("Enter your hostel/flat rent = "))
+food = int(input("Enter the amount of food ordered = "))
+electricity_spend = int(input("Electricity you've spent = "))
+charge_per_unit = int(input("Enter the charge per unit = "))
+persons = int(input("Enter the no of persons living in room/flat = "))
 
-# total_bill = electricity_spend * charge_per_unit
+total_bill = electricity_spend * charge_per_unit
 
 
 # # output will be:
